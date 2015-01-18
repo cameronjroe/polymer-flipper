c15c2260e2951d1fe83a048b5be1d03e979e3b3c
Cameron J Roe <cameronjroe@gmail.com>
2015-01-18 03:04:27 -0500

    add flip effect

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,43 +2,43 @@
 
 module.exports = function(grunt) {
 
-  require('load-grunt-tasks')(grunt);
+    require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-    sass: {
-      dev: {
-        options: {
-          includePaths: require('node-bourbon').includePaths
+    grunt.initConfig({
+        sass: {
+            dev: {
+                options: {
+                    includePaths: require('node-bourbon').includePaths
+                },
+                files: {
+                    'polymer-flipper.css': 'scss/polymer-flipper.scss',
+                    'css/demo.css': 'scss/demo.scss'
+                }
+            }
         },
-        files: {
-	  'css/style.css': 'scss/style.scss',
-	  'css/demo.css': 'scss/demo.scss'
+        connect: {
+            server: {
+                options: {
+                    base: '.',
+                    port: 3000
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            sass: {
+                files: 'scss/*.scss',
+                tasks: ['sass']
+            }
         }
-      }
-    },
-    connect: {
-      server: {
-        options: {
-          base: '.',
-          port: 3000
-        }
-      }
-    },
-    watch: {
-      options: {
-        livereload: true
-      },
-      sass: {
-	files: 'scss/*.scss',
-        tasks: ['sass']
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('default', [
-    'connect',
-    'sass',
-    'watch'
-  ]);
+    grunt.registerTask('default', [
+        'connect',
+        'sass',
+        'watch'
+    ]);
 
 };
